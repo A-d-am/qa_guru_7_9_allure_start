b7bf5b666b2edfd366509072ca4076c311ee68ec
A-d-a-m <adam3928@mail.ru>
2023-08-25 16:20:00 +0200

    added tests with @allure.step()

--- tests/test_repo_issue_name.py
+++ tests/test_repo_issue_name.py
@@ -5,6 +5,7 @@ from selene import browser, be, have
 expected_issue_name = 'Issue for homework tests'
 repo_name = 'repo:A-d-am/qa_guru_7_3'
 
+
 def test_repo_issue_name_only_selene():
     browser.open('/')
     browser.element('.header-search-button').click()
@@ -20,7 +21,7 @@ def test_repo_issue_name_only_selene():
 
 
 def test_repo_issue_name_with_allure_steps():
-    with allure.step('Открываем браузер'):
+    with allure.step('Открываем главную страницу github'):
         browser.open('/')
 
     with allure.step('Нажимаем на поле поиска'):
@@ -37,4 +38,33 @@ def test_repo_issue_name_with_allure_steps():
 
 
 def test_repo_issue_name_with_allure_decorator():
-    pass
+    open_github_main_page()
+    click_on_search_field()
+    search(repo_name)
+    go_to_issues_tab()
+    check_expected_issue_name(expected_issue_name)
+
+
+@allure.step('Открываем главную страницу github')
+def open_github_main_page():
+    browser.open('/')
+
+
+@allure.step('Нажимаем на поле поиска')
+def click_on_search_field():
+    browser.element('.header-search-button').click()
+
+
+@allure.step('Осуществляем поиск по названию репозитория "" {repo_name_to_search} ""')
+def search(repo_name_to_search: str):
+    browser.element('#query-builder-test').type(repo_name_to_search).press_enter()
+
+
+@allure.step('Переходим во вкладку Issues')
+def go_to_issues_tab():
+    browser.element('#issues-tab').click()
+
+
+@allure.step('Проверяем, что issue с названием "" {issue_name} "" есть в списке')
+def check_expected_issue_name(issue_name: str):
+    browser.element('#issue_4_link').should(have.text(issue_name)).should(be.visible)
