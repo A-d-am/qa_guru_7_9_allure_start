9baa9d66df7f7138267f72b59aa91e89a0b5c3bd
A-d-a-m <adam3928@mail.ru>
2023-08-25 15:08:31 +0200

    initial commit

---
+++ tests/test_repo_issue_name.py
@@ -0,0 +1,18 @@
+import pytest
+from selene import browser, be, have
+
+expected_issue_name = 'Issue for homework tests'
+
+
+def test_repo_issue_name_only_selene():
+    browser.open('/')
+    browser.element('.header-search-button').click()
+    browser.element('#query-builder-test').type('repo:A-d-am/qa_guru_7_3').press_enter()
+    browser.element('#issues-tab').click()
+
+    # вариант, когда мы знаем айди issue
+    browser.element('#issue_4_link').should(have.text(expected_issue_name))
+
+    # вариант, когда мы НЕ знаем айди issue
+
+    # browser.all('[aria-label="Issues"]').element_by(have.text(expected_issue_name))
