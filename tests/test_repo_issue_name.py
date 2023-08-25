06f913bedbd89b9065922cb15fa27f4aed103fbb
A-d-a-m <adam3928@mail.ru>
2023-08-25 15:42:49 +0200

    added tests with allure steps

--- tests/test_repo_issue_name.py
+++ tests/test_repo_issue_name.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from selene import browser, be, have
 
@@ -16,3 +17,24 @@ def test_repo_issue_name_only_selene():
     # вариант, когда мы НЕ знаем айди issue
 
     # browser.all('[aria-label="Issues"]').element_by(have.text(expected_issue_name)).should(be.visible)
+
+
+def test_repo_issue_name_with_allure_steps():
+    with allure.step('Открываем браузер'):
+        browser.open('/')
+
+    with allure.step('Нажимаем на поле поиска'):
+        browser.element('.header-search-button').click()
+
+    with allure.step('Осуществляем поиск по названию репозитория'):
+        browser.element('#query-builder-test').type(repo_name).press_enter()
+
+    with allure.step('Переходим во вкладку Issues'):
+        browser.element('#issues-tab').click()
+
+    with allure.step(f'Проверяем, что issue с названием {expected_issue_name} есть в списке'):
+        browser.element('#issue_4_link').should(have.text(expected_issue_name)).should(be.visible)
+
+
+def test_repo_issue_name_with_allure_decorator():
+    pass
